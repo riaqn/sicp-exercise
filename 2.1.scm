21c1c82944b6078ecd7283591873334ded13f2c8
riaqn <github@riaqn.com>
2015-09-09 18:43:50 +0800

    fix #1

--- 2.1.scm
+++ 2.1.scm
@@ -1,8 +1,9 @@
 (define make-rat
   (lambda (n d)
-    (if (negative? d)
-        (cons (- n) (- d))
-        (cons n d))))
+    (let ((g (gcd n d)))
+      (if (negative? d)
+          (cons (- n) (- d))
+          (cons n d)))))
 
 (make-rat -1 -2)
 (make-rat 1 -2)
